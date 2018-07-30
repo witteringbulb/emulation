8fdb494e2aba1a4a2d5137503e11a3a68cf81270
manfordt <manfordt@gmail.com>
2018-07-30 19:59:57 +0100

    Updated BranchGenerator so it can now create branches from supplied xy coordinates

--- src/main/java/neuron/branchGeneration/BranchGenerator.java
+++ src/main/java/neuron/branchGeneration/BranchGenerator.java
@@ -2,6 +2,7 @@ package neuron.branchGeneration;
 
 import neuron.branch.Branch;
 import neuron.factories.BranchFactory;
+import neuron.signal.SignalType;
 
 import java.util.List;
 
@@ -11,21 +12,20 @@ public class BranchGenerator<T extends Branch> {
     private final BranchFactory<T> branchFactory;
     private final Class branchType;
 
-    public BranchGenerator(double[] coordinatesOfStartOfBranch, Class<T> type) {
+    public BranchGenerator(double[] coordinatesOfStartOfBranch, Class<T> type, SignalType signalType) {
         if (coordinatesOfStartOfBranch.length != 2) {
-            throw new IllegalArgumentException("coordinates must be a n array of length 2");
+            throw new IllegalArgumentException("coordinates must be an array of length 2");
         }
         this.coordinatesOfStartOfBranch = coordinatesOfStartOfBranch;
-        this.branchFactory = new BranchFactory(type, coordinatesOfStartOfBranch);
+        this.branchFactory = new BranchFactory(type, coordinatesOfStartOfBranch, signalType);
         this.branchType = type;
     }
 
-    public List<T> createBranchesFromArraysOfEndpointCoordinates(double[] xCoordinates, double[] yCoordinates) {
-        //TODO: Remember to change BranchFactory and Branch builder to take xy coordinates of start and end, not length and orientation in radians
-        //TODO: Call factory method to create list
+    public List<Branch> createBranchesFromArraysOfEndpointCoordinates(double[] xCoordinates, double[] yCoordinates) {
+        return branchFactory.getBranchesFromArrayssOfEndpointCoordinates(xCoordinates, yCoordinates);
     }
 
-    public T generateBranch(double xCoordinateOfEndPoint, double yCoordinateOfEndPoint) {
+    public Branch generateBranch(double xCoordinateOfEndPoint, double yCoordinateOfEndPoint) {
         return this.branchFactory.getBranch(xCoordinateOfEndPoint, yCoordinateOfEndPoint);
     }
 
